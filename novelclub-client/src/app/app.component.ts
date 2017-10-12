8789004a12572d210ce953116781c11ffc9f0d3c
XvarX <yexangjun@qq.com>
2017-10-12 22:28:42 +0800

    connect the client and server

--- novelclub-client/src/app/app.component.ts
+++ novelclub-client/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app';
+
+  results: Object;
+  sUrl = "http://localhost:3000/test"
+
+  constructor(private http: HttpClient) {}
+  ngOnInit():void {
+    this.http.get(this.sUrl)
+             .subscribe(data => {
+               
+             });
+              
+  }
+  testconnect():void {
+    this.http.get(this.sUrl)
+            .subscribe(data => {
+              this.results = data;
+              console.log(this.results["results"]);
+    });
+  }
 }
--- novelclub-server/app.js
+++ novelclub-server/app.js
@@ -4,9 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require("cors");
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var test = require('./routes/routertest');
 
 var app = express();
 
@@ -21,9 +23,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/test', test);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
